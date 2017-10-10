b19129d7bfd1c91ee08a950e2cc3eefa3d56346e
Jessica <jessicalrenard@gmail.com>
2017-10-10 18:46:14 -0400

    Push

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Risky-Tripper',
+		profile: 'http://via.placeholder.com/200x250',
+		scores: [
+			'5',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'2',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Free-Faller',
+		profile: 'http://via.placeholder.com/200x250',
+		scores: [
+			'4',
+			'4',
+			'1',
+			'3',
+			'3',
+			'3',
+			'5',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Total-Homebody',
+		profile: 'http://via.placeholder.com/200x250',
+		scores: [
+			'2',
+			'2',
+			'5',
+			'5',
+			'1',
+			'1',
+			'3',
+			'1',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Lots-Of-Variety',
+		profile: 'http://via.placeholder.com/200x250',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'4',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Box-Of-Chocolates',
+		profile: 'http://via.placeholder.com/200x250',
+		scores: [
+			'1',
+			'4',
+			'5',
+			'2',
+			'5',
+			'3',
+			'2',
+			'5',
+			'3',
+			'4'
+		]
+	},
+];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,63 @@
+var path = require('path');
+
+// Import the list of beseecher entries
+var friendsData = require('../data/friends.js');
+
+// Export API routes
+module.exports = function(app) 
+  {
+  // console.log('___ENTER apiRoutes.js___');
+
+  // Total list of beseecher entries
+  app.get('/api/friends', function(req, res) 
+    {
+    res.json(friendsData);
+    });
+
+  // Add new beseecher entry
+  app.post('/api/friends', function(req, res) 
+    {
+    // setting the user input object
+    var userInput = req.body;
+    // console.log('userInput = ' + JSON.stringify(userInput));
+
+    var userResponses = userInput.scores;
+    // console.log('userResponses = ' + userResponses);
+
+    // Compute beseeching match
+    var matchName = '';
+    var matchProfile = '';
+    var totalDifference = 10000; // Initial value big for comparison
+
+    // Attain diff score by q for all beseechers
+    for (var i = 0; i < friendsData.length; i++) 
+    {
+      // console.log('friend = ' + JSON.stringify(friends[i]));
+      // Compute differences for each question
+      var diff = 0;
+      for (var j = 0; j < userResponses.length; j++) {
+        diff += Math.abs(friendsData[i].scores[j] - userResponses[j]);
+      } // math.abs for absolute value, ignoring + or -
+
+      // console.log('diff = ' + diff);
+
+      // If lowest difference, record the friend match
+      if (diff < totalDifference) 
+        {
+         console.log('Closest match found = ' + diff);
+         console.log('Friend name = ' + friendsData[i].name);
+         console.log('Friend profile = ' + friendsData[i].profile);
+
+         totalDifference = diff;
+         matchName = friendsData[i].name;
+         matchProfile = friendsData[i].profile;
+        }
+    }
+
+    // Add new user
+    friendsData.push(userInput);
+
+    // Send appropriate response
+    res.json({status: 'OK', matchName: matchName, matchProfile: matchProfile});
+  }); // closes app.post
+}; // closes module function
